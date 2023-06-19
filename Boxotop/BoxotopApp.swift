ef76cff258bb712db13bdabbd1cb505cc6407d3f
Maxime <maxime07800@hotmail.fr>
2023-06-19 07:21:12 +0200

    Initial Commit

---
+++ Boxotop/BoxotopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxotopApp.swift
+//  Boxotop
+//
+//  Created by Maxime on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoxotopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
